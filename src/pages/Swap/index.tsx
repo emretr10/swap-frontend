c8eda1c3b37a68203d396f20bb867d39cc1f1c0f
frankydev1 <frankydev1@protonmail.com>
2021-02-16 20:31:38 -0400

    new graphics

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -284,21 +284,16 @@ export default function Swap() {
     <>
       <div className="grid grid-cols-3 gap-4">
         <div>
-          <img className="h-16" src={require('../../assets/images/mochi4.png')}></img>
+          {/* <img className="h-26" src={require('../../assets/images/blank.png')}></img> */}
         </div>
-        <div>
-          <img className="h-16" src={require('../../assets/images/mochi3.png')}></img>
-        </div>
-        <div>
-          <img className="h-16" src={require('../../assets/images/mochi5.png')}></img>
-        </div>
-      </div>
-      <div className="msheader mt-3 mb-2 bg-fuchsia-500 text-2xl text-blue">
+        {/* <div className="msheader mt-3 mb-2 bg-fuchsia-500 text-2xl text-blue">
         MochiSwap - The tastiest Binance DEX exchange
       </div>
       <div className="mb-8 bg-fuchsia-800 h-12 rounded-md flex items-center justify-center text-2xl font-extrabold">
         Earn Mochi Tokens for each Swap!
+      </div> */}
       </div>
+ 
       {/* <h1 className="mb-8 font-normal text-3xl text-grey-darkest leading-loose my-3">
         MochiSwap - The tastiest Binance DEX exchange
       </h1> */}
@@ -310,6 +305,7 @@ export default function Swap() {
       <AppBody>
         <SwapPoolTabs active={'swap'} />
         <Wrapper id="swap-page">
+          <img className="h-23" src={require('../../assets/images/blank.png')}></img>
           <ConfirmSwapModal
             isOpen={showConfirm}
             trade={trade}
