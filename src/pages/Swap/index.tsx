5b3eb9b8c94f3556bbc56afed1be0a4dabd4aed8
frankydev1 <frankydev1@protonmail.com>
2021-02-16 20:13:11 -0400

    clean up css

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -296,7 +296,7 @@ export default function Swap() {
       <div className="msheader mt-3 mb-2 bg-fuchsia-500 text-2xl text-blue">
         MochiSwap - The tastiest Binance DEX exchange
       </div>
-      <div className="mb-8 bg-fuchsia-800 h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">
+      <div className="mb-8 bg-fuchsia-800 h-12 rounded-md flex items-center justify-center text-2xl font-extrabold">
         Earn Mochi Tokens for each Swap!
       </div>
       {/* <h1 className="mb-8 font-normal text-3xl text-grey-darkest leading-loose my-3">
